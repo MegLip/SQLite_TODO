b85182b8f380b2d01ebad65afc127859c31f6245
MegLip <70174916+MegLip@users.noreply.github.com>
2020-11-10 15:55:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 from forms import TodoForm
 from models import todos
+import sqlite3
 
 app = Flask(__name__)
 
@@ -23,21 +24,46 @@ def todos_list():
 @app.route("/todos/<int:todo_id>/", methods=["GET", "POST"])
 def todo_details(todo_id):
     todo = todos.get(todo_id)
-
     todo_dict = {
         'title': todo[1],
         'description': todo[2],
         'done': todo[3]
     }
-
     form = TodoForm(data=todo_dict)
 
+# aktualizuje określone id z bazy danych
     if request.method == "POST":
         if form.validate_on_submit():
             todos.update(todo_id, tuple(form.data.values())[:3])
         return redirect(url_for("todos_list"))
     return render_template("todo.html", form=form, todo_id=todo_id)
 
+# usuwa określone id z bazy danych
+@app.route("/todos/delete/<int:todo_id>", methods=["POST"])
+def delete_todo(todo_id):
+    todo = todos.get(todo_id)
+    todo_dict = {
+        'title': todo[1],
+        'description': todo[2],
+        'done': todo[3]
+    }
+    form = TodoForm(data=todo_dict)
+
+    conn = sqlite3.connect('database.db')
+   
+    checked = request.form.get("delete_checkbox")
+    if request.method == "POST":
+        if form.validate_on_submit():
+            if checked is None:
+                todos.update(todo_id, tuple(form.data.values())[:3])
+                print('status UPDATE: ', checked, todo_id, conn)
+            elif checked == '1':
+                todos.delete(conn, todo_id)
+                print('status: ', checked, todo_id, conn)
+        return redirect(url_for("todos_list"))
+    return render_template("todo.html", form=form, todo_id=todo_id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
